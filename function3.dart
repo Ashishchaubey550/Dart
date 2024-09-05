e88d7e7795513e94aa97e8ac58f843610b75f5c9
Ashish Chaubey <138882318+Ashishchaubey550@users.noreply.github.com>
2024-09-06 00:35:00 +0530

    06-09-2024

--- function3.dart
+++ function3.dart
@@ -1,15 +1,38 @@
+// void main() {
+//   var nameHai = printNAme();
+//   print(nameHai);
+//   var secondFunction = newName();
+//   print(secondFunction.$1.runtimeType);
+// }
+
+// int printNAme() {
+//   print("Ashish");
+//   return 12;
+// }
+
+// (int, String) newName() {
+//   return (14, "Ashish Chaubey");
+// }
+
+//*************************Funtion call Funtion********* */
+
+// void main() {
+//   var stuffs = myName();
+//   print(stuffs());
+// }
+
+// Function myName() {
+//   return () {
+//     print("Hello Here");
+//   };
+// }
+
+//*****************************Arrow function*********** */
+
 void main() {
-  var nameHai = printNAme();
-  print(nameHai);
-  var secondFunction = newName();
-  print(secondFunction);
+  final stuff = myName();
+  print(stuff);
 }
 
-int printNAme() {
-  print("Ashish");
-  return 12;
-}
-
-(int, String) newName() {
-  return (14, "Ashish Chaubey");
-}
+String myName() => "Age=>${12}";  
+// print is null return  value means print dont return any thing
